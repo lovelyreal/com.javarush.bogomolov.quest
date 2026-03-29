083cf136373f9474440acb5b13c624febb0f061a
Lovely <lovelyreal@mail.ru>
2026-03-29 13:02:24 +0300

    Добавил тест для Util, добавил jsp, jstl

--- src/main/java/controller/GreetingsServlet.java
+++ src/main/java/controller/GreetingsServlet.java
@@ -5,6 +5,7 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+
 import java.io.IOException;
 import java.io.PrintWriter;
 
@@ -13,12 +14,7 @@ public class GreetingsServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
-
-        // Пример: можно передать данные в JSP
-        req.setAttribute("message", "Добро пожаловать в историю!");
-
-        // Переход на JSP
         req.getRequestDispatcher("/WEB-INF/views/greetings.jsp")
                 .forward(req, resp);
     }
-    }
+}
